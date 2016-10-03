e41fe7280f80a9c3c4d99fefba95f496c5025e44
Angel Chen <rikugou@gmail.com>
2016-10-03 19:03:54 -0400

    Add routes for user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  get '/register', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   resources :users
-
-  get 'users/login'
-  get 'users/register' => 'users#new'
-
   resources :categories do
     resources :articles
   end
