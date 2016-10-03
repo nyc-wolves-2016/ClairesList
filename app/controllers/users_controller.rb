3b9faaed48663305209d17b637409bebc55be8ed
Angel Chen <rikugou@gmail.com>
2016-10-03 18:16:56 -0400

    Add user registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,19 @@
 class UsersController < ApplicationController
-  def show
-    @user = User.find(params[:id])
-  end
+  # def show
+  #   @user = User.find(params[:id])
+  # end
 
   def new
     @user = User.new
   end
 
+
+
   def edit
+  end
+
+  def create
+    @user = User.new(user_params)
   end
 
   def update
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users, only: [:new, :edit, :create]
+
+  get 'users/login'
+  get 'users/register' => 'users#new'
 
   resources :categories do
     resources :articles
