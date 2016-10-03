3a57b23b3fdda2da022e3daab101eabfbd18a879
Angel Chen <rikugou@gmail.com>
2016-10-03 19:17:49 -0400

    Add successful login page

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,17 @@
 module SessionsHelper
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def require_user
+    redirect '/login' unless logged_in?
+  end
 end
